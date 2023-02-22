fed3d43319caec0cb28a606fd5ac6820ddcfe229
beto-machado <luisrobertobeto3@gmail.com>
2023-02-22 09:27:34 -0300

    desafio 1

---
+++ desafio_ruby1.rb
@@ -0,0 +1,9 @@
+puts "\nDigite o seu nome:"
+username = gets.chomp
+puts "\nDigite sua idade:"
+year = gets.chomp
+
+
+puts "\n---------------------------------"
+
+puts "Seu nome é #{username}, e você tem #{year} anos"
